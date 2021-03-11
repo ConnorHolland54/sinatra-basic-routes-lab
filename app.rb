75806e8c8f80aad7c6bc1dbea4c082a39aa0f17a
Connor Holland <supersoccergoalie123@gmail.com>
2021-03-11 13:24:13 -0700

    commit tree 66f683ce043545921a19684abfdb4b2d74d83b9a

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do 
+        "Hello, World!"
+    end
+
+    get '/name' do 
+        "My name is Connor!"
+    end
+
+    get '/hometown' do 
+        "My hometown is Broomfield"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is Something"
+    end
 end
